451bd89400616471a4d68a976074d3d4865f8f13
Scott Dolphin <nathan-chen@csu.fullerton.edu>
2023-10-18 12:06:03 -0700

    fuck hort

--- script.py
+++ script.py
@@ -1,22 +1,10 @@
-def file_read(fileName):
-    songs = []
-    with open(fileName) as f:
-        for line in f:
-            url = line
-            if url[len(url) - 2:] == r'\n':
-                songs.append(url)
 
 
-            
-    
-    return songs
+songs = []
+with open('songs.txt') as f:
+    for line in f:
+       songs.append(line)
 
-
-myWord = 'Hello World!'
-lastTwo = myWord[len(myWord) -2:]
-print(myWord)
-print(lastTwo)
-print(myWord[:len(myWord) -2])
-
-# mySongs = file_read("songs.txt")
-# print(mySongs)
+for song in songs:
+    song
+    print(song.strip())
